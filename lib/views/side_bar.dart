edb2df6b9945abea2cd6fdc7bdaa7ff65e84697d
sairahul1526 <dravid.rahul1526@gmail.com>
2018-05-08 19:04:59 +0530

    side bar

---
+++ lib/views/side_bar.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class SideBar extends StatelessWidget {
+
+  String address = "100 Feet Rd-Koramangala, Bangalore, 560095";
+
+  @override
+    Widget build(BuildContext context) {
+      return new ListView(
+        children: <Widget>[
+          new ListTile(
+            title: new Text(address),
+            onTap: () => print("location clicked..."),
+          ),
+          new Divider(),
+          new Container(
+            padding: EdgeInsets.all(16.0),
+            child: new Text("My Information"),
+          ),
+          new ListTile(
+            title: new Text("Login"),
+            trailing: new Icon(Icons.arrow_right)
+          ),
+          new ListTile(
+            title: new Text("My Addresses"),
+            trailing: new Icon(Icons.arrow_right)
+          ),
+          new ListTile(
+            title: new Text("My Orders"),
+            trailing: new Icon(Icons.arrow_right)
+          ),
+          new ListTile(
+            title: new Text("My Cart"),
+            trailing: new Icon(Icons.arrow_right)
+          ),
+          new ListTile(
+            title: new Text("Offer Zone"),
+            trailing: new Icon(Icons.arrow_right)
+          ),
+          new Divider(),
+          new Container(
+            padding: EdgeInsets.all(16.0),
+            child: new Text("Others"),
+          ),
+          new ListTile(
+            title: new Text("Need Help?"),
+            trailing: new Icon(Icons.arrow_right)
+          ),
+          new ListTile(
+            title: new Text("Rate Us"),
+            trailing: new Icon(Icons.arrow_right)
+          ),
+          new ListTile(
+            title: new Text("Share"),
+            trailing: new Icon(Icons.arrow_right)
+          ),
+          new ListTile(
+            title: new Text("About Us"),
+            trailing: new Icon(Icons.arrow_right)
+          ),
+        ],
+      );
+    }
+}
